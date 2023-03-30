a1231c6b836d8f6c19dbd0aaf3a1e0da3ab57152
⚡️Disruptive Digital⚡️ <hello@disruptivedigital.eu>
2023-03-30 12:01:48 +0200

    Devnet DSSC
    erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqxhllllssz7sl7

--- src/config.devnet.ts
+++ src/config.devnet.ts
@@ -52,7 +52,7 @@ export const network: NetworkType = {
   gatewayAddress: 'https://devnet-gateway.multiversx.com',
   explorerAddress: 'http://devnet-explorer.multiversx.com',
   delegationContract:
-    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqx8llllsxavffq'
+    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqxhllllssz7sl7'
 };
 
 export const delegationContractData: DelegationContractType[] = [
